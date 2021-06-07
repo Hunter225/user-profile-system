a04d8da957b905f752bf1a6c5f5de97733813538
Hunter Sze <chsze@connect.ust.hk>
2021-06-08 00:45:50 +0800

    frontend init: miscellaneous

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,18 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-      .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }))
-      .catch((err) => err);
-  }
-
-  componentDidMount() {
-    this.callAPI();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.apiResponse}</p>
-      </div>
-    );
-  }
-}
-
-export default App;
+import { Switch, Route } from 'react-router-dom';
+import { Login } from './pages/user/Login';
+import { User } from './pages/user/User';
+import { Register } from './pages/user/Register';
+import { UserList } from './pages/adminUser/UserList';
+import { pageUrl } from './cosntant';
+export const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route path={pageUrl.user} component={User} />
+      <Route path={pageUrl.register} component={Register} />
+      <Route path={pageUrl.adminUser} component={UserList} />
+    </Switch>
+  );
+};
