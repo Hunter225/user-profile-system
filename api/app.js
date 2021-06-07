bc24f0917f923c495144c879b834f827be3e2b55
Hunter Sze <chsze@connect.ust.hk>
2021-06-07 11:39:49 +0800

    backend chore: miscellaneous

--- api/app.js
+++ api/app.js
@@ -1,30 +1,32 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var testAPIRouter = require("./routes/testAPI");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var adminUsersRouter = require('./routes/adminUsers');
+var testAPIRouter = require('./routes/testAPI');
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/testAPI", testAPIRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/admin/users', adminUsersRouter);
+app.use('/testAPI', testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -35,11 +37,28 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
+
+// init db connection
+const db = require('./models');
+const config = require('./config');
+
+db.mongoose
+  .connect(config.mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Successfully connect to MongoDB.');
+  })
+  .catch((err) => {
+    console.error('Connection error', err);
+    process.exit();
+  });
 
 module.exports = app;
